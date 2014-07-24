ce9412b91e1117e7a0c7581c340c556886dacd37
pravinz11 <pravin.singh11@gmail.com>
2014-07-24 14:08:12 +0530

    transformer placeholder
    transformer placeholder

--- msi/src/main/java/com/ww/msi/transformers/MsiTransformer.java
+++ msi/src/main/java/com/ww/msi/transformers/MsiTransformer.java
@@ -2,16 +2,21 @@ package com.ww.msi.transformers;
 
 import org.mule.api.MuleException;
 import org.mule.api.MuleMessage;
+import org.mule.api.context.MuleContextAware;
 import org.mule.api.lifecycle.Lifecycle;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
 
-public class MsiTransformer extends AbstractMessageTransformer{
+public class MsiTransformer extends AbstractMessageTransformer implements MuleContextAware{
 
+	
+	/**
+	 * JUST A PLACE HOLDER
+	 */
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding)
 			throws TransformerException {
-		Lifecycle lifecycle=(Lifecycle) message.getMuleContext().getRegistry().lookupFlowConstruct("abc");
+		Lifecycle lifecycle=(Lifecycle) muleContext.getRegistry().lookupFlowConstruct("abc");
 		try {
 			lifecycle.start();
 		} catch (MuleException e) {
